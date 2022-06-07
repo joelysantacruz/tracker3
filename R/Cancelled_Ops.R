ed2217ea96715df351209636806ecffbe3a86a41
jmsc65 <84806572+jmsc65@users.noreply.github.com>
2022-06-07 12:47:44 +0100

    Update Cancelled_Ops.R

--- R/Cancelled_Ops.R
+++ R/Cancelled_Ops.R
@@ -11,7 +11,7 @@ library(ggmap)
 
 ## PHS Cancelled Planned operations
 
-## 1. Cancelled operations by health board
+## 1. Cancelled operations by health board (CBHB)
 
 temp <- tempfile()
 source <- "https://www.opendata.nhs.scot/dataset/479848ef-41f8-44c5-bfb5-666e0df8f574/resource/0f1cf6b1-ebf6-4928-b490-0a721cc98884/download/cancellations_by_board_february_2022.csv"
@@ -19,7 +19,7 @@ temp <- curl_download(url=source, destfile=temp, quiet=FALSE, mode="wb")
 
 CBHB <- read.csv(temp)
 
-## 2. Cancelled operations Scotland
+## 2. Cancelled operations Scotland (CS)
 
 temp1 <- tempfile()
 source <- "https://www.opendata.nhs.scot/dataset/479848ef-41f8-44c5-bfb5-666e0df8f574/resource/df65826d-0017-455b-b312-828e47df325b/download/cancellations_scotland_february_2022.csv"
@@ -27,16 +27,13 @@ temp1 <- curl_download(url=source, destfile=temp1, quiet=FALSE, mode="wb")
 
 CS <- read.csv(temp1)
 
-
-## rename column in Scotland CSV to match Healthboard csv for merge
-
 names(CS)[names(CS) == 'Country'] <- 'HBT'
 
 ## Merge Scotland and individual boards
 cancelledops <- rbind(CBHB, CS)
 
 
-## Health board codes and remove extra columns for later merge with country code
+## Health board codes (healthboardid)
 temp2 <- tempfile()
 source <- "https://www.opendata.nhs.scot/dataset/9f942fdb-e59e-44f5-b534-d6e17229cc7b/resource/652ff726-e676-4a20-abda-435b98dd7bdc/download/hb14_hb19.csv"
 temp2 <- curl_download(url=source, destfile=temp2, quiet=FALSE, mode="wb")
@@ -44,7 +41,7 @@ temp2 <- curl_download(url=source, destfile=temp2, quiet=FALSE, mode="wb")
 healthboardid <- read.csv(temp2)
 healthboardid <- healthboardid[c("HB","HBName")]
 
-## Special health board codes and rename & remove columns for later merge
+## Special health board codes  (specialid)
 temp3 <- tempfile()
 source <- "https://www.opendata.nhs.scot/dataset/65402d20-f0f1-4cee-a4f9-a960ca560444/resource/0450a5a2-f600-4569-a9ae-5d6317141899/download/special-health-boards_19022021.csv"
 temp3 <- curl_download(url=source, destfile=temp3, quiet=FALSE, mode="wb")
@@ -56,7 +53,7 @@ names(specialid)[names(specialid) == 'SHBName'] <- 'HBName'
 specialid <- specialid[c("HB","HBName")]
 
 
-##country code and rename columns for merge with healthboard codes
+##country code Scotland (countryid)
 temp4 <- tempfile()
 source <- "https://www.opendata.nhs.scot/dataset/9f942fdb-e59e-44f5-b534-d6e17229cc7b/resource/9c6e6c56-2697-4184-92c6-60d69c2b6792/download/geography_codes_and_labels_country.csv"
 temp4 <- curl_download(url=source, destfile=temp4, quiet=FALSE, mode="wb")
@@ -65,7 +62,7 @@ countryid <- read.csv(temp4)
 names(countryid)[names(countryid) == 'Country'] <- 'HB'
 names(countryid)[names(countryid) == 'CountryName'] <- 'HBName'
 
-#combine lookup codes
+#combine lookup codes (lookups)
 lookups <- rbind(countryid, healthboardid, specialid)
 
 
@@ -87,7 +84,7 @@ cancelledops <- cancelledops %>%
 
 ##calculated fields: performed (planned - cancelled), and % calculations 
 ##(cancellations by type as % of all planned ops and as a % of canceled operations)
-##create unique identifier
+
 cancelledops <- cancelledops %>%
   mutate(Performed=TotalOperations-TotalCancelled) %>%
   mutate(Cancelled_By_Patient_pc_of_planned_ops=CancelledByPatientReason/TotalOperations*100) %>%
@@ -101,7 +98,7 @@ cancelledops <- cancelledops %>%
 
 
 
-## new data frame with selected columns
+## Final table (cancelledops)
 cancelledops <- cancelledops[c("Date2", "Month","Year","HBT","HBName","TotalOperations","TotalCancelled","Performed",
                                "CancelledByPatientReason","ClinicalReason","NonClinicalCapacityReason","OtherReason","Cancelled_By_Patient_pc_of_planned_ops",
                                "Cancelled_clinical_reason_pc_of_planned_ops","Non_clinical_capacity_reason_pc_of_planned_ops","Other_Reason_pc_of_planned_ops",
@@ -109,7 +106,7 @@ cancelledops <- cancelledops[c("Date2", "Month","Year","HBT","HBName","TotalOper
 
 
 
-## 3. Cancelled operations by hospital
+## 3. Cancelled operations by hospital (CBHOS)
 
 temp5 <- tempfile()
 source <- "https://www.opendata.nhs.scot/dataset/479848ef-41f8-44c5-bfb5-666e0df8f574/resource/bcc860a4-49f4-4232-a76b-f559cf6eb885/download/cancellations_by_hospital_march_2022.csv"
@@ -117,7 +114,7 @@ temp5 <- curl_download(url=source, destfile=temp5, quiet=FALSE, mode="wb")
 
 CBHOS <- read.csv(temp5)
 
-## Hospital codes
+## Hospital codes (hospitals)
 
 ##A&E sites
 
@@ -142,7 +139,7 @@ locations <- read.csv(temp7)
 locations <- locations[c("Location","Postcode","AddressLine","CA","XCoordinate","YCoordinate")] %>%
   filter(!is.na(XCoordinate)| !is.na(YCoordinate))
 
-## convert to lat / long
+## convert locations to lat / long
 
 locations <- st_as_sf(locations, coords = c("XCoordinate", "YCoordinate"), crs = 7405) ##EPSG:7405: OSGB36 / British National Grid + ODN height
 
@@ -152,7 +149,7 @@ locations <- locations %>%
           lat = st_coordinates(locations)[,2])
 
 
-## council areas
+## council areas (CA)
 
 temp8 <- tempfile()
 source <- "https://www.opendata.nhs.scot/dataset/9f942fdb-e59e-44f5-b534-d6e17229cc7b/resource/967937c4-8d67-4f39-974f-fd58c4acfda5/download/ca11_ca19.csv"
@@ -162,7 +159,7 @@ CA <- read.csv(temp8)
 
 CA <- CA[c("CA","CAName")]
 
-##joins
+##joins for hospitals
 
 names(hospitals)[names(hospitals) == 'TreatmentLocationCode'] <- 'Hospital'
 CBHOS <- CBHOS %>% 
@@ -178,7 +175,7 @@ CBHOS <- CBHOS %>%
 CBHOS <- CBHOS %>% 
   inner_join(CA)
 
-##date formatting
+##date formatting for hospitals
 
 names(CBHOS)[names(CBHOS) == 'Month'] <- 'Date1'
 CBHOS <- CBHOS %>%
@@ -189,7 +186,6 @@ CBHOS <- CBHOS %>%
 
 ##calculated fields: performed (planned - cancelled), and % calculations 
 ##(cancellations by type as % of all planned ops and as a % of canceled operations)
-##create unique identifier
 CBHOS <- CBHOS %>%
   mutate(Performed=TotalOperations-TotalCancelled) %>%
   mutate(Cancelled_By_Patient_pc_of_planned_ops=CancelledByPatientReason/TotalOperations*100) %>%
@@ -201,7 +197,7 @@ CBHOS <- CBHOS %>%
   mutate(Non_clinical_capacity_reason_pc_of_cancelled_ops=NonClinicalCapacityReason/TotalCancelled*100) %>%
   mutate(Other_Reason_pc_of_cancelled_ops=OtherReason/TotalOperations*100)
 
-## new data frame with selected columns
+## Data table (CBHOS)
 CBHOS <- CBHOS[c("Date2","Month","Year","TreatmentLocationName", "CurrentDepartmentType", "Status", "HBName","TotalOperations","TotalCancelled","Performed",
                  "CancelledByPatientReason","ClinicalReason","NonClinicalCapacityReason","OtherReason","Cancelled_By_Patient_pc_of_planned_ops",
                  "Cancelled_clinical_reason_pc_of_planned_ops","Non_clinical_capacity_reason_pc_of_planned_ops","Other_Reason_pc_of_planned_ops",
@@ -215,7 +211,7 @@ CBHOS <- CBHOS %>%
   mutate(filename=gsub(" ", "", TreatmentLocationName))  
 
 
-## filter to latest date for map base
+## filter to latest date (hospitalmapbase)
 hospitalmapbase <- CBHOS %>% 
   group_by(TreatmentLocationName) %>%
   filter(Date2==max(Date2))
@@ -226,10 +222,11 @@ hospitalmapbase <- CBHOS %>%
 write.csv(cancelledops, "data/Cancelled_Ops_by_HB.csv", row.names = FALSE)
 
 ## 2. hospital map base 
-write.csv(hospitalmapbase, "data/Cancelled_Ops_by_Hospital.csv", row.names = FALSE) ## all hospitals
+write.csv(hospitalmapbase, "data/Cancelled_Ops_by_Hospital.csv", row.names = FALSE) 
 
 ## 3. individual time series by hospital
-group_by(CBHOS, TreatmentLocationName) %>%
-  do(write_csv(., paste0(unique(.$filename), "data/_timeseries.csv")))
+group_by(CBHOS, filename) %>%
+  do(write_csv(., paste0(unique(.$filename), 
+                         file = "data/timeseries.csv")))
 
 
